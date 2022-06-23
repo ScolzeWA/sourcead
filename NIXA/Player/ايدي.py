4fa862ac27ae85760b44f6f259d96ad2f6492f73
ScolzeWA <64407840+ScolzeWA@users.noreply.github.com>
2022-06-23 17:23:39 +0200

    Add files via upload

--- NIXA/Player/ايدي.py
+++ NIXA/Player/ايدي.py
@@ -1,16 +1,34 @@
-from NIXA.main import bot
-from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message
+from config import BOT_USERNAME
 from NIXA.filters import command2, other_filters
+from NIXA.get_file_id import get_file_id
 
 
-@bot.on_message(filters.command2('ايدي","الايدي'))
-def ids(_, message):
-    reply = message.reply_to_message
-    if reply:
-        message.reply_text(
-            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ɪᴅ**: `{reply.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
-        )
-    else:
-        message.reply(
-            f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
-        )
+@Client.on_message(command2(["ايدي","الايدي"]))
+async def showid(_, message: Message):
+    await message.delete()
+    chat_type = message.chat.type
+
+    if chat_type == "private":
+        user_id = message.chat.id
+        await message.reply_text(f"<code>{user_id}</code>")
+
+    elif chat_type in ["group", "supergroup"]:
+        _id = ""
+        _id += "<b>ᴄʜᴀᴛ ɪᴅ</b>: " f"<code>{message.chat.id}</code>\n"
+        if message.reply_to_message:
+            _id += (
+                "<b>ʀᴇᴩʟɪᴇᴅ ᴜsᴇʀ ɪᴅ</b>: "
+                f"<code>{message.reply_to_message.from_user.id}</code>\n"
+            )
+            file_info = get_file_id(message.reply_to_message)
+        else:
+            _id += "<b>ᴜsᴇʀ ɪᴅ</b>: " f"<code>{message.from_user.id}</code>\n"
+            file_info = get_file_id(message)
+        if file_info:
+            _id += (
+                f"<b>{file_info.message_type}</b>: "
+                f"<code>{file_info.file_id}</code>\n"
+            )
+        await message.reply_text(_id)
